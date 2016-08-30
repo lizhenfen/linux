c7bc819b2e0a613886a91683c21ed5ceed4c384b
lizhenfen <743564797@qq.com>
2016-08-30 18:19:38 +0800

    重新排版了kvm

---
+++ 网站大全.sh
@@ -0,0 +1,93 @@
+openstack中文：  http://www.tuicool.com/search?kw=openstack
+openstack性能分析:
+    http://www.ibm.com/developerworks/cn/cloud/library/1606-automated-assessment-cloud-system/index.html
+    http://www.ibm.com/developerworks/cn/cloud/library/1603-openstack-chef-python-iaas/index.html
+ambari 和 kerberos:
+    http://www.ibm.com/developerworks/cn/opensource/os-cn-ambrri-kerberos/index.html
+    
+    
+    
+    http://download.csdn.net/detail/laoge/9522595
+ceph中文社区： http://docs.openfans.org/ceph/ceph4e2d658765876863/ceph-1/copy_of_intro-to-ceph30104ecb7ecdceph3011/intro-to-ceph30104ecb7ecdceph3011
+程序员必读书籍:                    http://developer.51cto.com/art/201602/506510.htm
+
+http://www.jikexueyuan.com/course/1713_2.html?ss=1
+
+
+http://www.qyule.me/7933/%E9%9A%94%E5%A3%81%E5%AE%B6%E5%BA%AD%E4%B8%BB%E5%A6%87-%E5%81%9A%E7%88%B1%E6%8A%80%E5%B7%A7%E8%B6%85%E6%A3%92-%E9%9F%B5%E5%91%B3%E7%8A%B9%E5%AD%98/
+
+官方网站：
+    tomcat：                        http://tomcat.apache.org/
+    Celery：                        http://celery.readthedocs.io/en/latest/index.html
+    django-rest-framework:          http://www.django-rest-framework.org/
+    SqlAlchemy:                     http://docs.sqlalchemy.org/en/rel_1_0/core/tutorial.html
+    haproxy                         http://cbonte.github.io/haproxy-dconv/ (github地址)
+    github:                         https://github.com/
+   
+    RedHat EPEL地址：               https://repos.fedorapeople.org/
+    Jekins官网：                    https://jenkins.io/index.html
+    阿里云镜像：                    http://mirrors.aliyun.com/
+   百度网盘                         http://www.sobaidupan.com/search.asp?r=0&wd=openstack二次开发
+
+个人学习：
+    猿课：                          http://i.apelearn.com/
+python: 
+    时间模块操作(time)              http://blog.chinaunix.net/uid-22671442-id-3527575.html
+开发系统：
+    编写自己的Traceroute(python):   https://blogs.oracle.com/ksplice/entry/learning_by_doing_writing_your
+    Cobbler API二次开发:            http://my.oschina.net/guopengfei/blog/633037?fromerr=fbAyMHmH
+    树莓派实用方案：                http://bbs.ickey.cn/community/forum.php?mod=forumdisplay&fid=117
+    微信python接口                  http://wechat-python-sdk.com/quickstart/official/
+    微信公众平台                    https://mp.weixin.qq.com/cgi-bin/loginpage?t=wxm2-login&lang=zh_CN
+  
+    python资料库: https://github.com/jobbole/awesome-python-cn
+    RESTFUL搭建物联网:  http://ebook.designiot.cn/
+博客系列：
+    好东西论坛                      http://forum.memect.com/
+    运维生存时间：                  http://www.ttlsa.com/
+    廖雪峰官网(python,git,javascript)：  http://www.liaoxuefeng.co
+    Openstack创建Windows镜像：      http://yunlzheng.github.io/2015/02/10/openstack-create-windows-images/
+    张戈博客：                      http://zhangge.net/#userconsent#
+    峰云,就它了                     http://xiaorui.cc/category/运维/
+    mysql数据库优化                 http://database.51cto.com/art/201506/478588.htm
+    docker中文博客                  http://www.widuu.com/docker_chinese
+    运维进行时 --刘天斯             http://blog.liuts.com/
+    架构师之路 --徐亮               http://www.xuliangwei.com/xubusi/280.html
+    snmp-监控 oid                   http://www.cnblogs.com/aspx-net/p/3554044.html
+    redis设计                       http://www.furion.info/
+    运维那些事儿                    http://www.07net02.com/?p=640(如何面试后端运维)
+    python开发                      http://www.cnblogs.com/wupeiqi/articles/5433893.html
+开源软件：
+    IDC云启装机平台：              http://www.idcos.com/store/cloudboot
+    cookie: http://www.dongwm.com/archives/guanyucookieyanjiu/
+    cpickle: http://www.dongwm.com/archives/guanyucpickleyanjiu/
+    gc: http://www.dongwm.com/archives/guanyugcyanjiu/  内存回收
+    cmd: http://www.dongwm.com/archives/guanyubz2yanjiu/
+    scapy                          http://www.secdev.org/projects/scapy/doc/index.html
+    pecan                          https://pecan.readthedocs.org/en/latest/installation.html
+    wsme                           https://pecan.readthedocs.org/en/latest/installation.html
+    piwik                          https://piwik.org/
+    IDM开发                        http://www.ibm.com/developerworks/cn/views/opensource/libraryview.jsp?sort_by=&show_abstract=true&show_all=&search_flag=&contentarea_by=Open+source&search_by=python+libvirt&topic_by=-1&type_by=所有类别&ibm-search=搜索
+    python打造定制化eclipse        http://developer.51cto.com/art/201603/506622.htm
+    django开发管理平台             http://www.ziqiangxuetang.com/django/django-cms-develop.html
+    ansible官网                    http://docs.ansible.com/ansible/intro_inventory.html
+    关注黑客和极客                 http://www.freebuf.com/sectool/76164.html
+    requests使用                   http://www.python-requests.org/en/master/user/quickstart/#make-a-request
+    cisco- snmp- lizhen666-lizhen666       http://snmp.cloudapps.cisco.com/Support/SNMP/do/BrowseMIB.do?local=en&step=2
+    sqlalchemy                     https://media.readthedocs.org/pdf/sqlalchemy/rel_1_0/sqlalchemy.pdf
+    web字体库                      http://www.bootcss.com/p/font-awesome/
+    jumpserver                     http://www.jumpserver.org/
+    
+    
+免费爬墙：
+    网址：                         http://hi18.cn/
+    蓝灯(lantern)                  https://github.com/getlantern/lantern
+免费大数据集：
+    政府网：                       http://www.data.gov/
+免费视频：
+    中国大学(MOOC,python)          http://www.icourse163.org/search.htm?search=python#/search
+    python免费视频(国外)：         https://www.udemy.com/courses/search/?ref=home&src=ukw&q=python&price=price-free&p=2
+    ELK初探：                    http://pan.baidu.com/s/1jItWoYy?qq-pf-to=pcqq.group#userconsent#path=%252F%25E6%25B7%2598%25E5%25AE%259D%25E5%258D%2596%25E8%25A7%2586%25E9%25A2%2591%252FELK%2520Stack%25E6%25B7%25B1%25E5%2585%25A5%25E6%25B5%2585%25E5%2587%25BA%25E8%25A7%2586%25E9%25A2%2591_GK&render-type=grid-view
+其他：
+    PDF转换Word 方案和软件破解：    http://jingyan.baidu.com/article/15622f244b812bfdfcbea529.html?qq-pf-to=pcqq.group#userconsent#
+    m/
